754e7c7ce07a95dda88770dfeac6ab3e7e7b5ede
Egbetola Sola <solaegbetola@gmail.com>
2022-06-23 11:15:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const aboutme = {
+    name: 'Egbetola Sola',
+    height: '6.1 inches',
+    country: 'Nigeria',
+};
+
+alert(
+    `My name is ${aboutme.name} , i am ${aboutme.height} tall and i  am from ${aboutme.country}.`
+);
